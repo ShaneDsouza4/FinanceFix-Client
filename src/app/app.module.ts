3b4670d21157cfb4c74da752c606b92d10cf9ffd
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-22 18:38:39 -0400

    Created Project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BudgetDetailsComponent } from './pages/budget-details/budget-details.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BudgetDetailsComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
